16bc05b878b627eab6ac95d2d044bcf1d9b93b38
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-07-18 18:07:13 +0900

    Timeクラスにコメントを追加

--- Template/Time.h
+++ Template/Time.h
@@ -3,23 +3,29 @@
 #include <Windows.h>
 #include <mmsystem.h>
 #include <time.h>
+#define DEFAULT_FPS (30.0)
 
 class Time : public Singleton<Time> {
 public:
 	friend class Singleton<Time>;
 	
+	//現在のFPSを取得します
 	int GetFPS();
+	//今現在アップデート出来るかを取得します(設定FPSを超えないようにしています)
 	bool IsUpdate();
 
-	//1フレーム前との時間の差を取得する
+	//1フレーム前との時間の差を取得します
 	double GetDeltaTime();
 
+	//スタートしてからのフレーム数を取得します
 	int GetFrameFromStart();
 
+	//スタートしてからの時間を取得します
+	float GetTimeFromStart();
 
-	//時間が流れる速さを変更する
+	//時間が流れる速さを変更します
 	 void SetTimeSpeed(float speed);
-	//時間が流れる速さを取得する
+	//時間が流れる速さを取得します
 	float GetTimeSpeed();
 
 private :
