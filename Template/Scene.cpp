2e4e4a386c1c3838084c72e68b098cd3a92ce764
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-09-18 12:17:54 +0900

    シーン周りを実装

---
+++ Template/Scene.cpp
@@ -0,0 +1,19 @@
+#include "Scene.h"
+#include "SceneManager.h"
+
+Scene::Scene(string _name) {
+	name = _name;
+	SceneManager::GetInstance().AddScene(this);
+}
+
+Scene::~Scene() {
+
+}
+
+void Scene::Start() {
+
+}
+
+void Scene::Load() {
+
+}
--- Template/SceneManager.h
+++ Template/SceneManager.h
@@ -1,9 +1,39 @@
 #pragma once
 #include "Singleton.h"
+#include "Scene.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class SceneManager : public Singleton<SceneManager>{
 public :
 	friend class Singleton<SceneManager>;
+
+	void LoadScene(Scene* scene);
+	void LoadScene(string name);
+	void LoadScene(unsigned int num);
+
+	void BackLoad(Scene* scene);
+	void BackLoad(string name);
+	void BackLoad(unsigned int num);
+
+	Scene* GetScene(unsigned int num);
+	Scene* GetScene(string name);
+
+	string GetSceneName(Scene* scene);
+	string GetSceneName(unsigned int number);
+
+	void AddScene(Scene* scene);
+
+	string GetNowScene();
+	int GetNowSceneNumber();
+
 private:
 	SceneManager();
 	~SceneManager();
+
+	Scene* nowScene;
+
+	vector<Scene*> sceneList;
 };
